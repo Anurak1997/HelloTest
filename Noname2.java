bd4b9fec158e0b4d55c45a938a0dda50bf8b261b
Anurak1997 <59598381+Anurak1997@users.noreply.github.com>
2020-03-03 14:10:08 +0700

    ขอแก้เดออ้าย

--- Noname2.java
+++ Noname2.java
@@ -1,8 +1,4 @@
-//----------------------------------------------------------
-//Author    : THAI OPEN CODE
-//Author URI: https://www.thaiopencode.com
-//Facebook  : https://www.facebook.com/ThaiOpenCode
-//----------------------------------------------------------
+
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -21,9 +17,7 @@ public class ArrayListDoubleLoop {
 			list.add(scan.nextDouble());
 			i++;
 		}
-		scan.close();
 		
-		Double sum = list.stream().mapToDouble(Double::doubleValue).sum();
 			
 	}
 }
